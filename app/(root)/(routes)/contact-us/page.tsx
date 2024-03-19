7a5dd57a32437945420190376336202b56ba9921
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-03-19 06:35:07 +0300

    update

--- app/(root)/(routes)/contact-us/page.tsx
+++ app/(root)/(routes)/contact-us/page.tsx
@@ -3,6 +3,12 @@ import { Metadata } from "next";
 import { Heading } from "@/components/heading";
 import { Section } from "@/components/section";
 import { FormContact } from "@/components/form-contact";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export const metadata: Metadata = {
   title: "MicroTech - Contact us",
@@ -26,32 +32,46 @@ export default function ContactUsPage() {
           description="Corporis eaque autem molestias blanditiis sequi eius rerum! Laboriosam asperiores porro nostrum maxime voluptatum, dolor, nulla nemo repellat nobis delectus deserunt dolorum!"
           className="text-center mx-auto max-w-5xl"
         />
-        {/* <Accordion type="single" collapsible className="mt-20 max-w-4xl mx-auto">
+        <Accordion
+          type="single"
+          collapsible
+          className="mt-20 max-w-4xl mx-auto"
+        >
           <AccordionItem value="item-1">
             <AccordionTrigger>sit amet consectetur.</AccordionTrigger>
             <AccordionContent>
-              Yes. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, voluptatibus officia blanditiis vitae ex quidem non distinctio quisquam corporis ad.
+              Yes. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Tempore, voluptatibus officia blanditiis vitae ex quidem non
+              distinctio quisquam corporis ad.
             </AccordionContent>
-          </AccordionItem >
+          </AccordionItem>
           <AccordionItem value="item-2">
-            <AccordionTrigger>ccessible Lorem ipsum dolor sit.?</AccordionTrigger>
+            <AccordionTrigger>
+              ccessible Lorem ipsum dolor sit.?
+            </AccordionTrigger>
             <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eaque blanditiis alias?
+              Yes. It adheres to the WAI-ARIA design pattern. Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Tenetur eaque blanditiis
+              alias?
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-3">
             <AccordionTrigger>ipsum. it accessible?</AccordionTrigger>
             <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eaque blanditiis alias?
+              Yes. It adheres to the WAI-ARIA design pattern. Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Tenetur eaque blanditiis
+              alias?
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-4">
-            <AccordionTrigger>Is it  Lorem ipsum dolor sit.</AccordionTrigger>
+            <AccordionTrigger>Is it Lorem ipsum dolor sit.</AccordionTrigger>
             <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eaque blanditiis alias?
+              Yes. It adheres to the WAI-ARIA design pattern. Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Tenetur eaque blanditiis
+              alias?
             </AccordionContent>
           </AccordionItem>
-        </Accordion> */}
+        </Accordion>
       </div>
     </Section>
   );
